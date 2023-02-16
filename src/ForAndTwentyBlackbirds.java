4922193a53211b78978a912a50d79aced13704bf
rbamarc <rbamarc929@gmail.com>
2023-02-16 06:48:10 -0800

    small changes

--- src/ForAndTwentyBlackbirds.java
+++ src/ForAndTwentyBlackbirds.java
@@ -5,12 +5,12 @@ public class ForAndTwentyBlackbirds {
      */
     public static void main(String[] args) throws Exception {
         int birdsInPie = 0;
-        for (int i = 0; i < 20; i++) {
+        for (int i = 1; i <= 24; i++) {
             System.out.println("Blackbirds #" + i + " goes into the pie");
             birdsInPie++;
         }
 
-        System.out.println("there are " + birdsInPie + " birds in there!");
+        System.out.println("There are " + birdsInPie + " birds in there!");
         System.out.println("Quite a full pie");
     }
 }
